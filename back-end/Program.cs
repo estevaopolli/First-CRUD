a595d547a795978045d919efda941fc2222edb42
Estevão:Win <estevao.polli@hotmail.com>
2026-05-27 17:03:14 -0300

    Integrando front-end com back-end

--- back-end/Program.cs
+++ back-end/Program.cs
@@ -1,9 +1,22 @@
+using System.Reflection.Metadata;
+using System.Security.Cryptography.X509Certificates;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("PermitirFrontEnd", policy =>
+    {
+        policy.AllowAnyOrigin()
+        .AllowAnyHeader()
+        .AllowAnyMethod();
+    });
+});
 
 var app = builder.Build();
 
@@ -16,29 +29,25 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+app.UseCors("PermitirFrontEnd");
 
-app.MapGet("/weatherforecast", () =>
+List<Registro> usuarios = new();
+app.MapGet("/usuarios", () =>
 {
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
+    return Results.Ok(usuarios);
+});
+
+app.MapPost("/usuarios", (Registro u) =>
+{
+    usuarios.Add(u);
+    return Results.Ok(u);
+});
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+public class Registro
 {
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    public string username {get; set;}
+    public string email {get; set;}
+    public string password {get; set;}
 }
+
